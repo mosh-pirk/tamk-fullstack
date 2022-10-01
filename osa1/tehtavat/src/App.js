ecf319b3aa2f70267f8cf575789e6a34d464d6b9
mosh-pirk <106318369+mosh-pirk@users.noreply.github.com>
2022-10-01 16:12:03 +0300

    tehtävä 1.4

--- osa1/tehtavat/src/App.js
+++ osa1/tehtavat/src/App.js
@@ -1,34 +1,32 @@
 const App = () => {
     const course = 'Half Stack application development'
 
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+
+    ]
 
     return (
         <div>
             <Header course={course}/>
-            <Content
-                part1={part1}
-                part2={part2}
-                part3={part3}
-            />
-            <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+            <Content parts={parts}/>
+            <Total parts={parts}/>
         </div>
     )
 }
 
 export default App;
-
 
 const Header = (props) => {
     return (
@@ -37,12 +35,11 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-    console.log(props)
     return (
        <div>
-           <Part part={props.part1}/>
-           <Part part={props.part2}/>
-           <Part part={props.part3}/>
+           {
+               props.parts.map( part => <Part part={part}/>)
+           }
        </div>
 
     )
@@ -56,10 +53,9 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-
     return (
         <div>
-            <p>Number of exercises {props.total}</p>
+            <p>Number of exercises { props.parts.reduce( (x, y) => x + y.exercises, 0)}</p>
         </div>
     )
 }
