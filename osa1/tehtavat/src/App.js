5262ce5853de7ef45c9f13fa8c6e255e0d7cfc11
mosh-pirk <106318369+mosh-pirk@users.noreply.github.com>
2022-10-02 20:00:11 +0300

    tehtävä 1.14*

--- osa1/tehtavat/src/App.js
+++ osa1/tehtavat/src/App.js
@@ -10,33 +10,38 @@ const App = () => {
         'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
         'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
     ]
-    const anecdotesNew = {...anecdotes.map(x => {
-            return {text: x, vote: 0}
-        })}
+    const anecdotesNew = anecdotes.map((x, i) => {
+        return {id: i, text: x, vote: 0}
+    })
 
     const [selected, setSelected] = useState(0)
-    const [anecdotesObject, setAnecdotesObject] = useState(anecdotesNew)
-    
+    const [mostVoted, setMostVoted] = useState(0)
+    const [anecdotesArray, setAnecdotesArray] = useState(anecdotesNew)
+
     const getRandomNumberBetween = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
     const handleVotes = (key) => {
-        const copy = {...anecdotesObject}
-        const vote = copy[key].vote
-        copy[key].vote = vote +1
-        setAnecdotesObject(copy)
+        const copy = [...anecdotesArray]
+        copy[key].vote = copy[key].vote + 1
+        setAnecdotesArray(copy)
+        const mostVoteditem = anecdotesArray.reduce((pre, cur) => pre.vote > cur.vote ? pre : cur)
+        setMostVoted(mostVoteditem.id)
     }
+
 
     return (
         <div>
-            {anecdotesObject[selected].text}
+            <Teksti text={'Anecdote of the day'} tyyppi={'bold'}/>
+            <Teksti text={anecdotesArray[selected].text} tyyppi={'newLine'}/>
+            <Teksti text={'has ' + anecdotesArray[selected].vote + ' votes '} tyyppi={'newLine'}/>
             <div>
-                 {'has ' + anecdotesObject[selected].vote + ' votes '}
+                <Button name={'vote'} handleClick={() => handleVotes(selected)}/>
+                <Button name={'nexanecdote'} handleClick={() => setSelected(getRandomNumberBetween(0, 7))}/>
             </div>
+            <Teksti text={'Anecdote with most votes'} tyyppi={'bold'}/>
+            <Teksti text={anecdotesArray[mostVoted].text} tyyppi={'newLine'}/>
+            <Teksti text={'has ' + anecdotesArray[mostVoted].vote + ' votes '} tyyppi={'newLine'}/>
 
-            <div>
-            <Button name={'vote'} handleClick={() => handleVotes(selected)} />
-            <Button name={'nexanecdote'} handleClick={() => setSelected(getRandomNumberBetween(0, 7))} />
-            </div>
         </div>
     )
 }
@@ -45,3 +50,9 @@ const App = () => {
 export default App;
 
 const Button = ({name, handleClick}) => <button onClick={() => handleClick()}> {name} </button>
+
+const Teksti = ({text, tyyppi}) => {
+    if (tyyppi === 'bold') return <h1>{text}</h1>
+    else if (tyyppi === 'newLine') return <div>{text}</div>
+    else return <span>{text}</span>
+}
