15148755ace9739bddba33531a9144ab4e075ad9
mosh-pirk <106318369+mosh-pirk@users.noreply.github.com>
2022-10-02 14:34:26 +0300

    tehtävä 1.8

--- osa1/tehtavat/src/App.js
+++ osa1/tehtavat/src/App.js
@@ -5,9 +5,6 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    const all = () => good + neutral + bad
-    const average = () => (good - bad) / all()
-    const positive = () => (good / all()) * 100
 
     return (
         <div>
@@ -15,20 +12,30 @@ const App = () => {
             <Button name={'good'} handleClick={() => setGood(good + 1)}/>
             <Button name={'neutral'} handleClick={() => setNeutral(neutral + 1)}/>
             <Button name={'bad'} handleClick={() => setBad(bad + 1)}/>
-            <Teksti text={'statics'} tyyppi={'bold'}/>
-            <Teksti text={'good ' + good} tyyppi={'newLine'}/>
-            <Teksti text={'neutral ' + neutral} tyyppi={'newLine'}/>
-            <Teksti text={'bad ' + bad} tyyppi={'newLine'}/>
-            <Teksti text={'all ' + all()} tyyppi={'newLine'}/>
-            <Teksti text={'average ' + average()} tyyppi={'newLine'}/>
-            <Teksti text={'positive ' + positive()} tyyppi={'newLine'}/>
-
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
 
 
 export default App;
+
+const Statistics = ({good, neutral, bad}) => {
+    const all = () => good + neutral + bad
+    const average = () => (good - bad) / all()
+    const positive = () => (good / all()) * 100
+
+    return <div>
+        <Teksti text={'statics'} tyyppi={'bold'}/>
+        <Teksti text={'good ' + good} tyyppi={'newLine'}/>
+        <Teksti text={'neutral ' + neutral} tyyppi={'newLine'}/>
+        <Teksti text={'bad ' + bad} tyyppi={'newLine'}/>
+        <Teksti text={'all ' + all()} tyyppi={'newLine'}/>
+        <Teksti text={'average ' + average()} tyyppi={'newLine'}/>
+        <Teksti text={'positive ' + positive()} tyyppi={'newLine'}/>
+    </div>
+
+}
 
 const Teksti = ({text, tyyppi}) => {
     if (tyyppi === 'bold') return <h1>{text}</h1>
