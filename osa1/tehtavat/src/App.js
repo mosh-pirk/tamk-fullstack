a4fda63e667f235c64df39f92b2b45e560fca757
mosh-pirk <106318369+mosh-pirk@users.noreply.github.com>
2022-10-08 12:40:25 +0300

    tehtävä 2.8

--- osa1/tehtavat/src/App.js
+++ osa1/tehtavat/src/App.js
@@ -2,9 +2,13 @@ import {useState} from "react";
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas'}
+        {
+            name: 'Arto Hellas',
+            phone: '0400 8989 897'
+        }
     ])
     const [newName, setNewName] = useState('')
+    const [newPhone, setNewPhone] = useState('')
 
     const handleAddPerson = (event) => {
         event.preventDefault()
@@ -14,14 +18,20 @@ const App = () => {
             return
         }
         const person = {
-            name: newName
+            name: newName,
+            phone: newPhone
         }
         if (newName.length > 1) setPersons(persons.concat(person))
         setNewName('')
+        setNewPhone('')
     }
 
     const handlePerson = (e) => {
         setNewName(e.target.value)
+    }
+
+    const handlePhone = (e) => {
+        setNewPhone(e.target.value)
     }
 
     return (
@@ -32,12 +42,17 @@ const App = () => {
                     name: <input value={newName} onChange={handlePerson}/>
                 </div>
                 <div>
+                    number: <input value={newPhone} onChange={handlePhone}/>
+                </div>
+                <div>
                     <button type="submit" onClick={handleAddPerson}>add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             <div>
-                {persons.map((person, i) => <p key={i}>{person.name}</p>)}
+                {persons.map((person, i) => <p key={i}>{
+                    `${person.name} ${person.phone}`
+                }</p>)}
             </div>
         </div>
     )
