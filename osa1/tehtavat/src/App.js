dbf970d1824dda810e5c3a31e1e08989e894d97d
mosh-pirk <106318369+mosh-pirk@users.noreply.github.com>
2022-10-09 12:11:01 +0300

    tehtävä 2.11

--- osa1/tehtavat/src/App.js
+++ osa1/tehtavat/src/App.js
@@ -1,15 +1,18 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456'},
-        {name: 'Ada Lovelace', number: '39-44-5323523'},
-        {name: 'Dan Abramov', number: '12-43-234345'},
-        {name: 'Mary Poppendieck', number: '39-23-6423122'}
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setFilter] = useState('')
+
+    const hook = () => {
+        axios.get('http://localhost:3001/persons')
+            .then(data => setPersons(data.data))
+    }
+
+    useEffect(hook, [])
 
     const handleAddPerson = (event) => {
         event.preventDefault()
